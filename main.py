b62ec3a1a8b87ff697d4e6e0bd6553b2947165da
Ian <ianmacbeth1@gmail.com>
2023-05-01 13:41:49 -0400

    multiple terms and adding differrnt die

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 import random
 
-# trying to upload to git
-# implement keep n highest/lowest
-# implement explode
-# implement adding different die types
+#TODO
+# keep n highest/lowest
+# explode
+# -adding different die types
+# reroll
+# save favorite rolls as "" to txt
+
+LAST_ROLL =[]
 
 def die(c, n):
     if c > 0:
@@ -12,18 +16,34 @@ def die(c, n):
 
 
 def roll(c):
-    for i in c:
-        term = i.split('d')
-        if not term[0].isnumeric():
-            term[0] = 1
-        print(i + ':', die(int(term[0]), int(term[1])))
+    for term in c:
+        #print('term:', term)
+        dice = term.split('+')
+        total = 0
+        result = []
+        for i in dice:
+            #print(i)
+            if i == '':
+                break
+            d = i.split('d')
+            if not d[0].isnumeric():
+                d[0] = 1
+            result.append(die(int(d[0]), int(d[1])))
+            total += result[-1]
+        print(term + ':', result, total)
+
 
 def main():
     cmd = input('>').split()
-    if cmd[0] == '/r':
+    if cmd[0] == '/r' or '/roll':
         print(roll(cmd[1:]))
-    elif cmd[0].lower() == 'exit' or 'x':
+    elif cmd[0].lower() == '/x' or '/exit' or 'x':
+        LAST_ROLL = cmd
         exit()
+    elif cmd[0].lower() == '/rr' or '/reroll':
+        print(roll(LAST_ROLL[1:]))
+    LAST_ROLL = cmd
     main()
+
 
 main()
